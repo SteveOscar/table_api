cf16b0c90b9df0887704860c2589912735c5f580
Steven Olson <steveoscaro@gmail.com>
2016-09-25 13:37:20 -0600

    complete basic user creation and automatic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -11,14 +11,16 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    # params[:id] is device id
+    @user = User.find_by(device: params[:id])
     render json: @user
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    create_params = JSON.parse(request.raw_post)
+    @user = User.new(create_params)
     if @user.save
       render json: @user
     else
